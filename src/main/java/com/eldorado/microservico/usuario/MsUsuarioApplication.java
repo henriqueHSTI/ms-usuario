63770fa4235421698810396413dc6070df7d4c3e
José Silva <henrique@hsti.eng.br>
2023-01-23 20:24:19 -0300

    Commit Inicial

---
+++ src/main/java/com/eldorado/microservico/usuario/MsUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado.microservico.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eldorado/microservico/usuario/MsUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldorado.microservico.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
